c72933f04756ed78cdbf0d76160a0602852afbcb
awadh <awadhkumarkumar421@gmail.com>
2024-06-11 14:13:34 +0000

    hello

---
+++ random1.py
@@ -0,0 +1,22 @@
+# import numpy as np 
+# a=np.arange(0,16)
+# print(a)
+# a.shape=(4,4)
+# print(a)
+# print("-------------")
+# print(a[3])
+# print(a[3,-1])
+
+
+# import numpy as np
+# a=np.random.randint(10,50,(5,5))
+# print(a)
+# print(np.mean(a))
+# b=np.arange(10,50)
+# print(np.sum(a))
+# print(np.std(a))
+
+# import numpy as np
+# a=np.linspace(1,10,20)
+# print(a)
+# print(a.reshape(4,5))
